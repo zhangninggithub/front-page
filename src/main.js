d13a7f0eddb8ebbf350785642717fc2d08e5fbd5
zhangning <786756496@qq.com>
2019-09-01 00:41:06 +0800

    初初始化项目

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册elementUI组件
 new Vue({
   router,
   render: h => h(App)
